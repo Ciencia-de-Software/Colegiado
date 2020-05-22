2ceeb192a141c0ea83359e1b4f2b12454b738f0f
Eduardo Lacerda <dudu.lacerda.dl@gmail.com>
2020-05-22 19:46:45 -0300

    versão 0.2.7.4

--- src/pages/projetos/npi.js
+++ src/pages/projetos/npi.js
@@ -2,6 +2,7 @@ import React from "react"
 import Cabecalho from "../../components/cabecalho/cabecalho"
 import Rodape from "../../components/rodape/rodape"
 import Npi from "../../images/npi-imagem.jpeg"
+import Ricado from "../../images/ricardo.jpg"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -9,6 +10,27 @@ import { fas } from '@fortawesome/free-solid-svg-icons'
 
 library.add(fab)
 library.add(fas)
+
+const Colaboradores = props => (
+    <>
+    <div class="titulo-aluno">
+        {props.aluno}
+        <a href={props.linkedin} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+        <a href={props.github} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github-square']} /></a>
+    </div>
+    <div class="corpo-aluno">
+        <p><span>Projeto Atual:</span> <a href={props.projetoatual} target="_blank" rel="noopener noreferrer">{props.titulotrabalho}</a></p>
+        <p><span>Descrição:</span> {props.resumo}</p>
+    </div>
+    <div class="botao-projeto">
+        <a href={props.pagprojetos} target="_blank" rel="noopener noreferrer">
+            <span class="botao-acessar">
+                Acessar Todos os Projetos
+            </span>
+        </a>
+    </div>
+    </>
+)
 
 export default () =>
     <>
@@ -46,127 +68,109 @@ export default () =>
                 </div>
                 <div class="div-trabalhos-npi">
                 <span class="titulo-trabalhos-npi">
-                    Trabalhos
+                    Projetos
                 </span>
                 </div>
                 <div class="div-conteudos-trabalhos">
                     <table>
                         <tr>
                             <td>
-                                <div class="titulo-aluno">
-                                    Ana Kataoka
-                                    <a href="https://www.linkedin.com/in/ana-kataoka-9370601a4/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-                                    <a href="https://github.com/AnaGabiK" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github-square']} /></a>
-                                </div>
-                                <div class="corpo-aluno">
-                                    <p><span>TRABALHO:</span> Colegiado UniFil</p>
-                                    <p><span>DESCRIÇÃO:</span> Projeto de criação do site para o Colegiado de Computação do Centro de Ensino Filadélfia (UniFil), utilizando Gatsby.</p>
-                                </div>
-                                <div class="botao-projeto">
-                                    <a href="https://gitlab.com/npi-unifil/site-npi" target="_blank" rel="noopener noreferrer">
-                                        <span class="botao-acessar">
-                                            Acessar
-                                        </span>
-                                    </a>
-                                </div>
+                                <Colaboradores
+                                    aluno = "Ana Kataoka"
+                                    linkedin = "https://www.linkedin.com/in/ana-kataoka-9370601a4/"
+                                    github = "https://github.com/AnaGabiK"
+                                    projetoatual = "https://gitlab.com/npi-unifil/site-npi"
+                                    titulotrabalho = "Sistema Web do Colegiado de Computação UniFil"
+                                    resumo = "Projeto de criação do site para o Colegiado de Computação do Centro de Ensino Filadélfia (UniFil), utilizando Gatsby."
+                                    pagprojetos = "https://gitlab.com/npi-unifil"
+                                />
                             </td>
                             <td>
-                                <div class="titulo-aluno">
-                                    Eduardo Lacerda
-                                    <a href="https://www.linkedin.com/in/dudulacerda/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-                                    <a href="https://github.com/dudulacerdadl" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github-square']} /></a>
-                                </div>
-                                <div class="corpo-aluno">
-                                    <p><span>TRABALHO:</span> Colegiado UniFil</p>
-                                    <p><span>DESCRIÇÃO:</span> Projeto de criação do site para o Colegiado de Computação do Centro de Ensino Filadélfia (UniFil), utilizando Gatsby.</p>
-                                </div>
-                                <div class="botao-projeto">
-                                    <a href="https://gitlab.com/npi-unifil/site-npi" target="_blank" rel="noopener noreferrer">
-                                        <span class="botao-acessar">
-                                            Acessar
-                                        </span>
-                                    </a>
-                                </div>
+                                <Colaboradores
+                                    aluno = "Eduardo Lacerda"
+                                    linkedin = "https://www.linkedin.com/in/dudulacerda/"
+                                    github = "https://github.com/dudulacerdadl"
+                                    projetoatual = "https://gitlab.com/npi-unifil/site-npi"
+                                    titulotrabalho = "Sistema Web do Colegiado de Computação UniFil"
+                                    resumo = "Projeto de criação do site para o Colegiado de Computação do Centro de Ensino Filadélfia (UniFil), utilizando Gatsby."
+                                    pagprojetos = "https://gitlab.com/npi-unifil"
+                                />
                             </td>
                         </tr>
                         <tr>
                             <td>
-                                <div class="titulo-aluno">
-                                    Maria Fernanda
-                                    <a href="https://www.linkedin.com/in/maria-fernanda-vaz-romero-51b369184/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-                                    <a href="https://github.com/VazMF" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github-square']} /></a>
-                                </div>
-                                <div class="corpo-aluno">
-                                    <p><span>TRABALHO:</span> Clonagem do Hacker News</p>
-                                    <p><span>DESCRIÇÃO:</span> Clonagem do site Hacker News como forma de estudo e treino das habilidades em HTML5 e CSS3.</p>
-                                </div>
-                                <div class="botao-projeto">
-                                    <a href="https://github.com/VazMF/CEV-HTML5" target="_blank" rel="noopener noreferrer">
-                                        <span class="botao-acessar">
-                                            Acessar
-                                        </span>
-                                    </a>
-                                </div>
+                                <Colaboradores
+                                    aluno = "Maria Fernanda"
+                                    linkedin = "https://www.linkedin.com/in/maria-fernanda-vaz-romero-51b369184/"
+                                    github = "https://github.com/VazMF"
+                                    projetoatual = "https://github.com/VazMF/CEV-HTML5"
+                                    titulotrabalho = "Clonagem do Hacker News"
+                                    resumo = "Clonagem do site Hacker News como forma de estudo e treino das habilidades em HTML5 e CSS3."
+                                    pagprojetos = "https://gitlab.com/npi-unifil"
+                                />
                             </td>
                             <td>
-                                <div class="titulo-aluno">
-                                    Nome do Aluno 
-                                    <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-                                    <a href="https://github.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github-square']} /></a>
-                                </div>
-                                <div class="corpo-aluno">
-                                    <p><span>TRABALHO:</span> Nome do trabalho</p>
-                                    <p><span>DESCRIÇÃO:</span> Esse espaço é dedicado a descrição do trabalho apresentado, escreva sobre o que é o trabalho e caso queira um comentário adicional sobre.</p>
-                                </div>
-                                <div class="botao-projeto">
-                                    <a href="https://gitlab.com/" target="_blank" rel="noopener noreferrer">
-                                        <span class="botao-acessar">
-                                            Acessar
-                                        </span>
-                                    </a>
-                                </div>
+                                <Colaboradores
+                                    aluno = "Nome do Aluno"
+                                    linkedin = "https://www.linkedin.com/"
+                                    github = "https://github.com/"
+                                    projetoatual = "https://gitlab.com/"
+                                    titulotrabalho = "Nome do trabalho"
+                                    resumo = "Esse espaço é dedicado a descrição do trabalho apresentado, escreva sobre o que é o trabalho e caso queira um comentário adicional sobre."
+                                    pagprojetos = "https://gitlab.com/npi-unifil"
+                                />
                             </td>
                         </tr>
                         <tr>
                             <td>
-                                <div class="titulo-aluno">
-                                    Nome do Aluno 
-                                    <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-                                    <a href="https://github.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github-square']} /></a>
-                                </div>
-                                <div class="corpo-aluno">
-                                    <p><span>TRABALHO:</span> Nome do trabalho</p>
-                                    <p><span>DESCRIÇÃO:</span> Esse espaço é dedicado a descrição do trabalho apresentado, escreva sobre o que é o trabalho e caso queira um comentário adicional sobre.</p>
-                                </div>
-                                <div class="botao-projeto">
-                                    <a href="https://gitlab.com/" target="_blank" rel="noopener noreferrer">
-                                        <span class="botao-acessar">
-                                            Acessar
-                                        </span>
-                                    </a>
-                                </div>
+                                <Colaboradores
+                                    aluno = "Nome do Aluno"
+                                    linkedin = "https://www.linkedin.com/"
+                                    github = "https://github.com/"
+                                    projetoatual = "https://gitlab.com/"
+                                    titulotrabalho = "Nome do trabalho"
+                                    resumo = "Esse espaço é dedicado a descrição do trabalho apresentado, escreva sobre o que é o trabalho e caso queira um comentário adicional sobre."
+                                    pagprojetos = "https://gitlab.com/npi-unifil"
+                                />
                             </td>
                             <td>
-                                <div class="titulo-aluno">
-                                    Nome do Aluno 
-                                    <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-                                    <a href="https://github.com/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github-square']} /></a>
-                                </div>
-                                <div class="corpo-aluno">
-                                    <p><span>TRABALHO:</span> Nome do trabalho</p>
-                                    <p><span>DESCRIÇÃO:</span> Esse espaço é dedicado a descrição do trabalho apresentado, escreva sobre o que é o trabalho e caso queira um comentário adicional sobre.</p>
-                                </div>
-                                <div class="botao-projeto">
-                                    <a href="https://gitlab.com/" target="_blank" rel="noopener noreferrer">
-                                        <span class="botao-acessar">
-                                            Acessar
-                                        </span>
-                                    </a>
-                                </div>
+                                <Colaboradores
+                                    aluno = "Nome do Aluno"
+                                    linkedin = "https://www.linkedin.com/"
+                                    github = "https://github.com/"
+                                    projetoatual = "https://gitlab.com/"
+                                    titulotrabalho = "Nome do trabalho"
+                                    resumo = "Esse espaço é dedicado a descrição do trabalho apresentado, escreva sobre o que é o trabalho e caso queira um comentário adicional sobre."
+                                    pagprojetos = "https://gitlab.com/npi-unifil"
+                                />
                             </td>
                         </tr>
                     </table>
                 </div>
+                <table class="div-conteudos-trabalhos">
+                    <tr>
+                        <td>
+                            <div class="titulo-professor">
+                                Ricardo Inacio Alvares e Silva
+                                <a href="https://www.linkedin.com/in/ricardo-inacio-alvares-e-silva-32a3b023" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
+                            </div>
+                            <div class="corpo-professor">
+                                <p><span>Coordenador do Projeto</span></p>
+                                <p>Professor adjunto  da UniFil - Centro Universitário Filadélfia, Bacharel em Ciência da Computação pela Pontifícia Universidade Católica de Minas Gerais (PUC-MG), e mestre, também em Ciência da Computação, pela Universidade Estadual de Londrina (UEL), quando foi bolsista CAPES.</p>
+                            </div>
+                            <div class="botao-projeto">
+                                <a href="http://lattes.cnpq.br/6856943558243501" target="_blank" rel="noopener noreferrer">
+                                    <span class="botao-acessar">
+                                        Acessar Lattes
+                                    </span>
+                                </a>
+                            </div>
+                        </td>
+                        <td>
+                            <img class="imagem-professor" src={Ricado} alt="NPI"/>
+                        </td>
+                    </tr>                    
+                </table>
                 <div class="atendimento-npi">
                     Atendimento
                 </div>
