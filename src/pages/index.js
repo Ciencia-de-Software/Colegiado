4ffd630f2f4dce92deae9611ce99b312722207b3
Eduardo Lacerda <dudu.lacerda.dl@gmail.com>
2020-07-07 10:44:55 -0300

    versão 0.4

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,9 @@ import Rodape from "../components/rodape/rodape"
 
 export default () =>
     <>
+    <title>Colegiado de Computação - UniFil</title>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         <Cabecalho />
         <div class="pag">
             <p class="titulo-index"><strong class="titulo-comp">Colegiado de Computação</strong></p>
